8e974960cb702e357a68d230484f8e16b1b383fc
shimly <575801327@qq.com>
2016-05-23 10:10:26 +0800

    项目初始化

---
+++ iReader/iReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iReader
+//
+//  Created by shimly on 16/5/23.
+//  Copyright © 2016年 shimly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
